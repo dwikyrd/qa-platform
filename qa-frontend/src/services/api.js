d4e7ff0adf0f3a2af4256a3ea5808a44e9df5492
Dwiky <dwikyreziandino@gmail.com>
2026-06-11 16:58:35 +0700

    change baseUrls api.js

--- qa-frontend/src/services/api.js
+++ qa-frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create axios instance with base config
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Sesuaikan dengan port Flask Anda
+  baseURL: "", // Sesuaikan dengan port Flask Anda
   headers: {
     "Content-Type": "application/json",
   },
